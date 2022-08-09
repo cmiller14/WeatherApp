80cb405284c396867d97f290c86a4d64084372d9
Chase Miller <a02283751@aggies.usu.edu>
2022-08-08 20:57:43 -0600

    added volley dependency

---
+++ app/src/main/java/com/a02283751/weatherapp/viewmodel/GetWeatherInfo.java
@@ -0,0 +1,11 @@
+package com.a02283751.weatherapp.viewmodel;
+
+import com.android.volley.RequestQueue;
+
+public class GetWeatherInfo {
+
+    private RequestQueue queue;
+
+    private String string;
+
+}
