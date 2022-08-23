f1efbf38177b90f1369231874298e645e649ec30
Chase Miller <a02283751@aggies.usu.edu>
2022-08-23 17:54:45 -0600

    api for weather

--- app/src/main/java/com/a02283751/weatherapp/viewmodel/GetWeatherInfo.java
+++ app/src/main/java/com/a02283751/weatherapp/viewmodel/GetWeatherInfo.java
@@ -1,11 +1,21 @@
 package com.a02283751.weatherapp.viewmodel;
 
-import com.android.volley.RequestQueue;
 
-public class GetWeatherInfo {
+import android.app.Application;
 
-    private RequestQueue queue;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class GetWeatherInfo extends AndroidViewModel {
+
+    private RequestQueueSingleton queue = RequestQueueSingleton.getInstance(
+            this.getApplication().getApplicationContext());
 
     private String string;
+    private String url;
 
+    public GetWeatherInfo(@NonNull Application application) {
+        super(application);
+        url = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+    }
 }
